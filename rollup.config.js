242c3cf34141a794f3220320d5a004ee969692c8
slipdawg <69774592+slipdawg@users.noreply.github.com>
2020-08-16 17:40:02 -0700

    Add files via upload

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import rust from "@wasm-tool/rollup-plugin-rust";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+    input: {
+        background: "src/background/Cargo.toml",
+        chart: "src/chart/Cargo.toml",
+        popup: "src/popup/Cargo.toml",
+        records: "src/records/Cargo.toml",
+        saltybet: "src/saltybet/Cargo.toml",
+        twitch_chat: "src/twitch_chat/Cargo.toml",
+    },
+    output: {
+        dir: "static/js",
+        format: "esm",
+        sourcemap: true,
+    },
+    plugins: [
+        rust({
+            serverPath: "js/",
+            importHook: function (path) {
+                return "chrome.runtime.getURL(" + JSON.stringify(path) + ")";
+            },
+        }),
+        //terser(),
+    ],
+};
